90a405ad63abda6d28d1123bef07ac0536326f68
ohho-developer <ohho.developer@gmail.com>
2025-10-05 19:32:21 +0900

    db schema add

--- main_project/settings/base.py
+++ main_project/settings/base.py
@@ -78,6 +78,9 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', ''),
         'PORT': os.environ.get('DB_PORT', ''),
+        'OPTIONS': {
+            'options': os.environ.get('DB_SCHEMA', '')
+        }
     }
 }
 
