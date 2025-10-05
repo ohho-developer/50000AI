7e1c5d7d83ac5babf11de1c5b04c5be9057d16cc
ohho-developer <ohho.developer@gmail.com>
2025-10-06 00:54:32 +0900

    requirement sentence-transformers add

--- main_project/settings/development.py
+++ main_project/settings/development.py
@@ -11,12 +11,12 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 # Development database (SQLite)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Development email backend
